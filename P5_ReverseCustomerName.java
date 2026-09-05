eb293d1dce718accf58ad74836c5bbb6ed06f8d5
Arnab Mahata <arb.mahata2727@gmail.com>
2026-09-05 13:45:14 +0530

    Add customer name reversal functionality
    Implement a method to reverse a customer's name and display it.

---
+++ P5_ReverseCustomerName.java
@@ -0,0 +1,21 @@
+public class P5_ReverseCustomerName {
+
+    public static String reverseCustomerName(String customerName) {
+        char[] originalChars = customerName.toCharArray();
+        char[] reversedChars = new char[originalChars.length];
+
+        for (int i = 0; i < originalChars.length; i++) {
+            reversedChars[i] = originalChars[originalChars.length - 1 - i];
+        }
+
+        return new String(reversedChars);
+    }
+
+    public static void main(String[] args) {
+        String customerName = "Sunil";
+        String reversedName = reverseCustomerName(customerName);
+
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Reversed Name: " + reversedName);
+    }
+}
